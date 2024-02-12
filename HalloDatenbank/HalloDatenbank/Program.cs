1613b088a814de8db782ae6c020223c9c9648dd5
André <AndreR@ppedv.de>
2024-02-12 10:28:17 +0100

    Db Verbindung hergestellt

---
+++ HalloDatenbank/HalloDatenbank/Program.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Data.SqlClient;
+
+Console.WriteLine("Hello Database");
+
+var conString = "Server=(localdb)\\mssqllocaldb;Database=NORTHWND;Trusted_Connection=true;TrustServerCertificate=true;";
+//var conString = "Server=.;Database=NORTHWiND;Trusted_Connection=true;TrustServerCertificate=true;";
+
+try
+{
+    var con = new SqlConnection(conString);
+    con.Open();
+    Console.WriteLine("DB Verbindung OK");
+}
+catch (SqlException ex)
+{
+    Console.WriteLine($"Datenbankfehler: {ex.ErrorCode} - {ex.Message}");
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Fehler: {ex.Message}");
+}
