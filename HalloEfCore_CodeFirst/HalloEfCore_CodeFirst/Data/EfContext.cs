909b5e6a45bd87ab0368fc29cbb0fd82afa819d7
André <AndreR@ppedv.de>
2024-02-14 15:17:35 +0100

    Mehr EF

--- HalloEfCore_CodeFirst/HalloEfCore_CodeFirst/Data/EfContext.cs
+++ HalloEfCore_CodeFirst/HalloEfCore_CodeFirst/Data/EfContext.cs
@@ -9,6 +9,7 @@ namespace HalloEfCore_CodeFirst.Data
         public DbSet<Person> People { get; set; }
         public DbSet<Customer> Customers { get; set; }
         public DbSet<Employee> Employees { get; set; }
+        public DbSet<Department> Departments { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
@@ -20,8 +21,12 @@ namespace HalloEfCore_CodeFirst.Data
             //modelBuilder.Entity<Person>().ToTable("Persons");
             modelBuilder.Entity<Person>().UseTpcMappingStrategy();
             modelBuilder.Entity<Person>().Property(x => x.Name).HasMaxLength(99);
-            modelBuilder.Entity<Customer>().ToTable("Customers");
-            modelBuilder.Entity<Employee>().ToTable("Employees");
+
+            //modelBuilder.Entity<Customer>().ToTable("Customers");
+            //modelBuilder.Entity<Employee>().ToTable("Employees");
+
+            modelBuilder.Entity<Employee>().HasMany(x => x.Customers).WithOne(x => x.SupportAgent).OnDelete(DeleteBehavior.Cascade);
+            //modelBuilder.Entity<Customer>().HasOne(x => x.SupportAgent).WithMany(x => x.Customers);
         }
     }
 }
--- HalloEfCore_CodeFirst/HalloEfCore_CodeFirst/Model/Person.cs
+++ HalloEfCore_CodeFirst/HalloEfCore_CodeFirst/Model/Person.cs
@@ -1,23 +1,39 @@
-﻿using System.Globalization;
+﻿using System.ComponentModel.DataAnnotations;
+using System.Globalization;
 
 namespace HalloEfCore_CodeFirst.Model
 {
     public abstract class Person
     {
         public int Id { get; set; }
+
+        [MaxLength(77)]
         public string Name { get; set; } = string.Empty;
         public DateTime BirthDate { get; set; }
     }
 
     public class Customer : Person
     {
+        [MaxLength(55)]
         public string CustomerNr { get; set; } = string.Empty;
-        public virtual Employee? SupportAgent { get; set; } 
+
+        [EmailAddress]
+        public string Email { get; set; } = string.Empty;
+        public virtual Employee? SupportAgent { get; set; }
     }
 
     public class Employee : Person
     {
         public string Job { get; set; } = string.Empty;
         public virtual ICollection<Customer> Customers { get; set; } = new HashSet<Customer>();
+        public virtual ICollection<Department> Departments { get; set; } = new HashSet<Department>();
+    }
+
+    public class Department
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public virtual ICollection<Employee> Employees{ get; set; } = new HashSet<Employee>();
+
     }
 }
