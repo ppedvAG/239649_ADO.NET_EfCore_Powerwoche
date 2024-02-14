d9bb8534ce3edd57e4e668bb533d5da286db0eba
André <AndreR@ppedv.de>
2024-02-14 14:05:31 +0100

    Tolle Emplyees Blazor App

---
+++ HalloEfCore_CodeFirst/HalloEfCore_CodeFirst/Data/EfContext.cs
@@ -0,0 +1,27 @@
+﻿using HalloEfCore_CodeFirst.Model;
+using Microsoft.EntityFrameworkCore;
+using System.Diagnostics;
+
+namespace HalloEfCore_CodeFirst.Data
+{
+    public class EfContext : DbContext
+    {
+        public DbSet<Person> People { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Employee> Employees { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var conString = "Server=(localdb)\\mssqllocaldb;Database=EfCore_CodeFirst;Trusted_Connection=true;";
+            optionsBuilder.UseLazyLoadingProxies().UseSqlServer(conString).LogTo(Console.WriteLine).EnableSensitiveDataLogging(true);
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //modelBuilder.Entity<Person>().ToTable("Persons");
+            modelBuilder.Entity<Person>().UseTpcMappingStrategy();
+            modelBuilder.Entity<Person>().Property(x => x.Name).HasMaxLength(99);
+            modelBuilder.Entity<Customer>().ToTable("Customers");
+            modelBuilder.Entity<Employee>().ToTable("Employees");
+        }
+    }
+}
---
+++ HalloEfCore_CodeFirst/HalloEfCore_CodeFirst/Model/Person.cs
@@ -0,0 +1,23 @@
+﻿using System.Globalization;
+
+namespace HalloEfCore_CodeFirst.Model
+{
+    public abstract class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public DateTime BirthDate { get; set; }
+    }
+
+    public class Customer : Person
+    {
+        public string CustomerNr { get; set; } = string.Empty;
+        public virtual Employee? SupportAgent { get; set; } 
+    }
+
+    public class Employee : Person
+    {
+        public string Job { get; set; } = string.Empty;
+        public virtual ICollection<Customer> Customers { get; set; } = new HashSet<Customer>();
+    }
+}
