9c44db63a8dd8db096692a5c66de3394a438a32d
André <AndreR@ppedv.de>
2024-02-15 12:32:21 +0100

    Besser mit Test DB arbeiten!!

--- ppedv.AdventureApp/ppedv.AdventureApp.Data.EfCore.Tests/AdventureWorks2019ContextTests.cs
+++ ppedv.AdventureApp/ppedv.AdventureApp.Data.EfCore.Tests/AdventureWorks2019ContextTests.cs
@@ -8,11 +8,10 @@ namespace ppedv.AdventureApp.Data.EfCore.Tests
         public void CanReadProduct()
         {
             var builder = new DbContextOptionsBuilder<AdventureWorks2019Context>();
-            builder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=AdventureWorks2019;Trusted_Connection=true;Encrypt=True;TrustServerCertificate=true");
+            builder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=AdventureWorks2019_DEV;Trusted_Connection=true;Encrypt=True;TrustServerCertificate=true");
             AdventureWorks2019Context con = new AdventureWorks2019Context(builder.Options);
 
-            //AdventureWorks2019Context con = new AdventureWorks2019Context();
-            var prod = con.Employees.FirstOrDefault();
+            var prod = con.Products.FirstOrDefault();
 
             Assert.NotNull(prod);
         }
